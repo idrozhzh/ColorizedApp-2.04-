5a741fc9b8aec69fcacf4d460babaa93fd9fa6e6
i_drozhzh <idrozhzh@gmail.com>
2022-06-27 17:04:40 +0300

    Edits after homework feedback

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var colorRect: UIView!
+    @IBOutlet weak var colorRectView: UIView!
     
     @IBOutlet weak var redColorIndexLabel: UILabel!
     @IBOutlet weak var greenColorIndexLabel: UILabel!
@@ -18,57 +18,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var redColorSlider: UISlider!
     @IBOutlet weak var greenColorSlider: UISlider!
     @IBOutlet weak var blueColorSlider: UISlider!
-   
-    private var bgColor = UIColor(red: 200, green: 100, blue: 50, alpha: 1)
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorRect.layer.cornerRadius = 20
-        colorRect.backgroundColor = bgColor
-        //Не понимаю почему задаю цвет, а он затирает тот, что установлен в IB и тот, что устанавливаю я кодом и ставит все равно белый при загрузке приложения. Можете объяснить, что я тут сделал не так?
+        colorRectView.layer.cornerRadius = 20
         
-        setupColorSlider(for: redColorSlider)
-        setupColorSlider(for: greenColorSlider)
-        setupColorSlider(for: blueColorSlider)
+        sliderAction()
     }
     
-    @IBAction func sliderAction(_ sender: UISlider) {
-        let currColor = UIColor(
+    @IBAction func sliderAction() {
+        colorRectView.backgroundColor = UIColor(
             red: CGFloat(redColorSlider.value),
             green: CGFloat(greenColorSlider.value),
             blue: CGFloat(blueColorSlider.value),
             alpha: 1
         )
         
-        colorRect.backgroundColor = currColor
-        
-        switch sender {
-            case redColorSlider:
-                redColorIndexLabel.text = String(format: "%.2f", sender.value)
-            case greenColorSlider:
-                greenColorIndexLabel.text = String(format: "%.2f", sender.value)
-            default:
-                blueColorIndexLabel.text = String(format: "%.2f", sender.value)
-        }
-    }
-    
-    // MARK: private methods
-    private func setupColorSlider(for slider: UISlider) {
-        guard let colorComponents = bgColor.cgColor.components else { return }
-        
-        switch slider {
-            case redColorSlider:
-            slider.value = Float(colorComponents[0] / 255)
-            redColorIndexLabel.text = String(format: "%.2f", colorComponents[0] / 255)
-        case greenColorSlider:
-            slider.value = Float(colorComponents[1] / 255)
-            greenColorIndexLabel.text = String(format: "%.2f", colorComponents[1] / 255)
-        default:
-            slider.value = Float(colorComponents[2] / 255)
-            blueColorIndexLabel.text = String(format: "%.2f", colorComponents[2] / 255)
-        }
+        redColorIndexLabel.text = String(format: "%.2f", redColorSlider.value)
+        greenColorIndexLabel.text = String(format: "%.2f", greenColorSlider.value)
+        blueColorIndexLabel.text = String(format: "%.2f", blueColorSlider.value)
     }
     
 
