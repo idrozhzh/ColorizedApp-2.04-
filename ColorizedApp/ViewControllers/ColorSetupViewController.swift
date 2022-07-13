aad3905632bb0a4bd076d5c5381a82f3f0233b30
i_drozhzh <idrozhzh@gmail.com>
2022-07-13 10:46:24 +0300

    Setup new ViewController Added textFields to color setup added new methods to ColorSetupViewController Setup data transfer from MainVC to ColorSetupVC Planed last steps to finish the app

--- ColorizedApp/ViewControllers/ColorSetupViewController.swift
+++ ColorizedApp/ViewControllers/ColorSetupViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class ColorSetupViewController: UIViewController {
+    
+    /*
+     Осталось сделать
+     1. Кнопка Done
+     2. Подписать мэинВС под протокол для передачи данных
+     3. Реализовать метод передачи данных
+     4. Настроить клавиатуру для ТФ
+     5. Добавить кнопку Done клавиатуре
+     6. Настроить сворачивание клавиатуры
+     7. Переписать функцию с текстовых полей по ТЗ
+     8. Проверить работу приложения
+     9. Почистить код
+     
+     */
 
     @IBOutlet weak var colorRectView: UIView!
     
@@ -19,12 +33,20 @@ class ColorSetupViewController: UIViewController {
     @IBOutlet weak var greenColorSlider: UISlider!
     @IBOutlet weak var blueColorSlider: UISlider!
     
+    @IBOutlet weak var redColorTF: UITextField!
+    @IBOutlet weak var greenColorTF: UITextField!
+    @IBOutlet weak var blueColorTF: UITextField!
+    
+    
+    var currentColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorRectView.layer.cornerRadius = 20
         
-        sliderAction()
+        colorRectView.backgroundColor = currentColor
+        viewSetup()
     }
     
     @IBAction func sliderAction() {
@@ -36,7 +58,30 @@ class ColorSetupViewController: UIViewController {
         )
         
         sliderIndexLabelsSetup()
+        sliderTextFieldsSetup()
+        
+        currentColor = colorRectView.backgroundColor
     }
+
+    @IBAction func textFieldAction(_ sender: UITextField) {
+        guard let textFieldValue = sender.text else { return }
+        guard let floatTFValue = Float(textFieldValue) else { return }
+        
+        switch sender {
+        case redColorTF:
+            redColorSlider.value = floatTFValue
+        case greenColorTF:
+            greenColorSlider.value = floatTFValue
+        default:
+            blueColorSlider.value = floatTFValue
+        }
+        
+        sliderAction()
+    }
+}
+
+//MARK: private methods
+extension ColorSetupViewController {
     
     private func sliderIndexLabelsSetup() {
         redColorIndexLabel.text = String(format: "%.2f", redColorSlider.value)
@@ -44,7 +89,27 @@ class ColorSetupViewController: UIViewController {
         blueColorIndexLabel.text = String(format: "%.2f", blueColorSlider.value)
     }
     
-
-
+    private func sliderTextFieldsSetup() {
+        redColorTF.text = String(format: "%.2f", redColorSlider.value)
+        greenColorTF.text = String(format: "%.2f", greenColorSlider.value)
+        blueColorTF.text = String(format: "%.2f", blueColorSlider.value)
+    }
+    
+    private func sliderViewSetup() {
+        guard
+            let colorComponents = currentColor.cgColor.components
+        else { return }
+        
+        var count = 0
+        for slider in [redColorSlider, greenColorSlider, blueColorSlider] {
+            slider?.value = Float(colorComponents[count])
+            count += 1
+        }
+    }
+    
+    private func viewSetup() {
+        sliderViewSetup()
+        sliderIndexLabelsSetup()
+        sliderTextFieldsSetup()
+    }
 }
-
---
+++ ColorizedApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  ColorizedApp
+//
+//  Created by Иван Дрожжин on 13.07.2022.
+//
+
+import UIKit
+
+protocol ColorSetupViewControllerDelegate {
+    func saveColorSetup(with color: UIColor)
+}
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard
+            let colorSetupVC = segue.destination as? ColorSetupViewController
+        else { return }
+        
+        colorSetupVC.currentColor = view.backgroundColor
+    }
+    
+
+}
